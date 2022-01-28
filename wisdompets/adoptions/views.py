0ae5d600c708a2b4dc441a84e56525ae774eeeb7
punithguptha <saipunithbonagiri@gmail.com>
2022-01-28 23:39:30 +0530

    View Related Commit 1

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<div>Home View</div>')
+
+
+def pet_detail(request,pet_id):
+    return HttpResponse(f'<div>pet_detail view with id {pet_id}</div>')
