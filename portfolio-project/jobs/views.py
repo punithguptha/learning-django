6da8908142960fccfa57a56e9897e568c483ce2e
punithguptha <saipunithbonagiri@gmail.com>
2022-02-03 21:52:31 +0530

    Second Commit

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 def home(request):
+    jobs=Job.objects.all()
     # Django automatically refers content from the templates folder, so we dont need to mention it explicitly..So we are using jobs/sai.html to refer to the content
-    return render(request,"jobs/home.html")
+    return render(request,"jobs/home.html",{'jobs':jobs})
