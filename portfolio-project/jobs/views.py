21b675300d0d813aa772c42af0924b4cfcc646f2
punithguptha <saipunithbonagiri@gmail.com>
2022-02-04 22:11:01 +0530

    Creating Detailed Job View along with some other improvements

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 
 # Create your views here.
@@ -7,3 +7,7 @@ def home(request):
     jobs=Job.objects.all()
     # Django automatically refers content from the templates folder, so we dont need to mention it explicitly..So we are using jobs/sai.html to refer to the content
     return render(request,"jobs/home.html",{'jobs':jobs})
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request,"jobs/job_info.html",{"job_detail":job_detail})
