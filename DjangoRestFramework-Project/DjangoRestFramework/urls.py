3935e15853e68ac8c6d92bcb4332e66979efaeb9
punithguptha <saipunithbonagiri@gmail.com>
2022-02-13 18:35:49 +0530

    Initial Views and Template Related Changes

--- DjangoRestFramework-Project/DjangoRestFramework/urls.py
+++ DjangoRestFramework-Project/DjangoRestFramework/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/<int:id>/',store.views.show, name='show-product'),
+    path('cart/',store.views.cart,name='shopping-cart'),
+    path('',store.views.index, name='list-products')
 ]
 
 
