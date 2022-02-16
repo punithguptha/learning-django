1e31c7ff7c95aeaf46d32b1d8f0685cefc907442
punithguptha <saipunithbonagiri@gmail.com>
2022-02-17 00:14:58 +0530

    Enabling full text search filter backend

--- DjangoRestFramework-Project/store/api_views.py
+++ DjangoRestFramework-Project/store/api_views.py
@@ -2,14 +2,17 @@ from rest_framework.generics import ListAPIView
 from store.serializers import ProductSerializer
 from store.models import Product
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 # https://www.django-rest-framework.org/api-guide/generic-views/ Refer to this to understand about generic views that drf provides and the params that are available for each
 class ProductList(ListAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
-    filter_backends=(DjangoFilterBackend,) #This can also be declared at the global level or at view level like this
+    filter_backends=(DjangoFilterBackend,SearchFilter) #This can also be declared at the global level or at view level like this
     filter_fields=('id',) #The options to specify by which the filtering should occur..Hit the endpoint to understand more(Like this http://127.0.0.1:8000/api/v1/products?id=1)
-
+    # One can also set the type of the search to be made..By default it is Partial Match..The other options available are Exact Match and Regular Expression
+    # Refer to this https://www.django-rest-framework.org/api-guide/filtering/#searchfilter for more details on the same
+    search_fields=('name','description')
     #To filter products based on if they are on sale or not..For this we have to override the get_queryset method
     # Look into queryset.filter in django to understand more
     def get_queryset(self):
