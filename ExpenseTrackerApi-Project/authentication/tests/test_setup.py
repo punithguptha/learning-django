5a6ff0cd0de0d9aad8d68dca1709137ae7508fa5
Sai Punith Bonagiri <saipunithbonagiri@gmail.com>
2022-03-08 00:11:37 +0530

    Utilising Faker to generate dummy data for tests

--- ExpenseTrackerApi-Project/authentication/tests/test_setup.py
+++ ExpenseTrackerApi-Project/authentication/tests/test_setup.py
@@ -1,14 +1,18 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from faker import Faker
 
 class TestSetup(APITestCase):
     def setUp(self):
         self.register_url=reverse('register')
         self.login_url=reverse('login')
+        self.fake_data_generator=Faker()
+        Faker.seed(0)
         self.user_data={
-            'email':'testuser@gmail.com',
-            'username':'testuser',
-            'password':'testuserpass',
+            'email': self.fake_data_generator.email(),
+            'username': self.fake_data_generator.first_name(),
+            # https://faker.readthedocs.io/en/master/providers/faker.providers.misc.html?highlight=password#faker.providers.misc.Provider.password
+            'password': self.fake_data_generator.password(length=20,special_chars=False),
         }
         return super().setUp()
 
